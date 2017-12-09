ff9e2c56d5967d7b5561953e72ceffab0ef28832
amir <123>
2017-12-09 17:16:30 +1100

    changed message

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express(); // creates a single express application
 // get() creates a route handler for http get method
 // '/' is the default route
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ from: 'amir', to: 'ava', message: 'hey ava, you look cute :)' });
 });
 
 const PORT = process.env.PORT || 5000;
