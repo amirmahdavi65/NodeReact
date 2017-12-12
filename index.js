bdda3859161f0d770be35f408556958c9b5f74da
amir <123>
2017-12-12 23:23:44 +1100

    google auth added

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require ('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express(); // creates a single express application
 
-// get() creates a route handler for http get method
-// '/' is the default route
-app.get('/', (req, res) => {
-  res.send({ from: 'amir', to: 'ava', message: 'hey ava, you look cute :)' });
-});
+// this strategy injects an object with 'google' identifier
+// so passport knows how to authenticate using this id below
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      console.log('access token', accessToken);
+      console.log('refresh token', refreshToken);
+      console.log('profile:', profile);
+    }
+  )
+);
+
+// use google strategy to handle this route and authenticate
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+  scope: ['profile', 'email']
+}));
+
+app.get(
+  '/auth/google/callback',
+  passport.authenticate('google')
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
