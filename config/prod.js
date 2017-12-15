c6e15092f351afb9a5e26388ab1bc9d97877eb11
amir <123>
2017-12-16 00:16:23 +1100

    hopefully it was just a typo

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 // these are all pulled from heroku environment variables, safe to commit to git
 module.exports = {
-  googleClientID: process.env.env.GOOGLE_CLIENT_ID,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY // just some random string
