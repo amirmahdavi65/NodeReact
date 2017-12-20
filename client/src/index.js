40f14c35642e744f4f5163b88cb8cbe0aeb8254d
Amir Mahdavi <amirmahdavi65@gmail.com>
2017-12-20 23:03:48 +1100

    added a basic component

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDom.render(
+  <provider store={store}><App /></provider>,
+  document.querySelector('#root')
+);
