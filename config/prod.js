f40bd9bd65168819eaf28ec030d701bebcbb9f80
Amir Mahdavi <amirmahdavi65@gmail.com>
2019-05-28 23:28:38 +1000

    added stripe api keys and hid them in .env.development and .dev.production

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY // just some random string
+  cookieKey: process.env.COOKIE_KEY, // just some random string
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
