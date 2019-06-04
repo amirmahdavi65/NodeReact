d22a3ab5d775089eadfc8919ad8b96c2e89c75b8
Amir Mahdavi <amirmahdavi65@gmail.com>
2019-06-04 21:11:40 +1000

    make sure express serves up all react assets

--- index.js
+++ index.js
@@ -38,8 +38,20 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
+// every request will sequentially try the following checks until it's served
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
+
+if (process.env.NODE_ENV === 'production') {
+  // express serves up production assets
+  app.use(express.static('client/build'));
+
+  // express serves index.html if it doesn't recognize the path
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', index.html));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
