30a58e1ea196958e900029bd9580d98440e246f2
Amir Mahdavi <amirmahdavi65@gmail.com>
2019-06-04 22:29:56 +1000

    Should've listened to react docs and commited the .env files; Just make sure there is no secret in them.

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ if (process.env.NODE_ENV === 'production') {
   // express serves index.html if it doesn't recognize the path
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', index.html));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
