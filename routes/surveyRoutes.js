69bd370d138417ceec6b5e02ded77685b2a1feba
Amir Mahdavi <amirmahdavi65@gmail.com>
2019-06-06 13:45:10 +1000

    Added survey post api and sendgrid mail helper

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,21 +1,35 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
-const requireCredits = require('../middlewares/requireCredits');
+const requireCredit = require('../middlewares/requireCredit');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+  app.post('/api/surveys', requireLogin, requireCredit, async (req, res) => {
     const { title, subject, body, recipients } = req.body;
     const survey = new Survey({
       title,
       subject,
       body,
-      recipients: recipients.split(',').map(item => {
-        email: item.trime();
-      }),
+      recipients: recipients.split(',').map(item => ({
+        email: item.trim()
+      })),
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
+
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits--;
+      const user = await req.user.save();
+      res.send(user);
+    } catch (err) {
+      res.status(422).send(err);
+    }
   });
 };
