d7091b856a2ec7674307b56ef8c98192a516039f
Amir Mahdavi <amirmahdavi65@gmail.com>
2019-06-06 17:09:39 +1000

    Added a new Dashboard component with an 'add' button to navigate to the SurveyNew component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,8 +5,8 @@ import * as actions from '../actions';
 
 import Header from './Header';
 import Landing from './Landing';
-const Dashboard = () => <h2>Dashboard</h2>;
-const SurveyNew = () => <h2>SurveyNew</h2>;
+import Dashboard from './Dashboard';
+import SurveyNew from './surveys/SurveyNew';
 
 class App extends Component {
   componentDidMount() {
@@ -26,6 +26,9 @@ class App extends Component {
       </div>
     );
   }
-};
+}
 
-export default connect(null, actions)(App);
+export default connect(
+  null,
+  actions
+)(App);
