35ecd2ac67d2fae7697baa640f5e1b565bfc63f3
Amir Mahdavi <amirmahdavi65@gmail.com>
2019-06-07 11:15:24 +1000

    Created form from reusable fields

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
-import { reduxForm } from 'redux-form';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+
+const fields = [
+  { label: 'Survey Title', name: 'title' },
+  { label: 'Subject Line', name: 'subject' },
+  { label: 'Email Body', name: 'body' },
+  { label: 'Recipient List', name: 'emails' }
+];
 
 class SurveyForm extends Component {
+  renderFields() {
+    return (
+      <div>
+        {fields.map((item, index) => (
+          <Field key={index} type="text" component={SurveyField} {...item} />
+        ))}
+      </div>
+    );
+  }
   render() {
-    return <div>Survey Form</div>;
+    return (
+      <div>
+        <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
+          {this.renderFields()}
+          <Link to="/surveys" className="red btn-flat white-text">
+            Cancel
+          </Link>
+          <button type="submit" className="teal btn-flat right white-text">
+            Next <i className="material-icons right">navigate_next</i>
+          </button>
+        </form>
+      </div>
+    );
   }
 }
 
